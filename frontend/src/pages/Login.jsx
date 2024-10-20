7e19a3fa4e0a67d70d46a89cf21f21eb0af96e05
ekremsekmen <ekremsekmenq@gmail.com>
2024-10-20 19:09:09 +0300

    qq

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
-// src/pages/Login.jsx
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuthStore from '../store/authStore';
+import '../styles/Login.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -12,39 +12,38 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError(null); // Reset error state before login attempt
+    setError(null);
     try {
       await login(username, password);
-      navigate('/admin'); // Navigate to admin page if login is successful
+      navigate('/admin');
     } catch (error) {
-      setError(error.message); // Set error message for display
+      setError(error.message);
     }
   };
 
   return (
-    <div>
-      <h2>IWiew Admin Panel</h2>
-    <div className="login-container">
-      
-      <form onSubmit={handleSubmit}>
-        {error && <p className="error-message">{error}</p>} {/* Display error message */}
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="login-page">
+      <div className="login-container">
+        <h2>IWiew Admin Panel</h2>
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error-message">{error}</p>}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
